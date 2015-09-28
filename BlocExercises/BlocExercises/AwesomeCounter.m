faaa368f171478eb7e4900587605a3f767d4300b
Ryan Walker <ryan@maddenwalker.com>
2015-09-28 16:17:36 -0700

    Assignment 12 Loops, completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,32 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    //this code seems to be too process intensive as I am performing the same logic twice; wondering if it would be more beneficial to use a simple if/else statement
+    NSInteger lowerBound = (number < otherNumber) ? number : otherNumber;
+    NSInteger upperBound = (otherNumber < number ) ? number : otherNumber;
+    
+    //same implementation as above with more lines of code, but with one comparison happening
+//    NSInteger lowerBound;
+//    NSInteger upperBound;
+//     
+//    if (number < otherNumber) {
+//        lowerBound = number;
+//        upperBound = otherNumber;
+//    } else {
+//        lowerBound = otherNumber;
+//        upperBound = number;
+//    }
+    
+    NSMutableString *rangeOfNumbers = [@"" mutableCopy];
+    
+    while (lowerBound <= upperBound) {
+        [rangeOfNumbers appendString:[NSString stringWithFormat:@"%d",lowerBound]];
+        lowerBound++;
+        NSLog(@"%@", rangeOfNumbers);
+    }
+    
+    return rangeOfNumbers;
 }
 
 @end
